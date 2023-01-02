de10d631375a997591f0fe10040c8e32370f993e
Mohsen Mirhoseini <msn.mirhoseini@gmail.com>
2023-01-02 10:42:59 +0100

    Initial Kotlin Native project

---
+++ src/linuxArm32HfpMain/kotlin/com/mohsenoid/Main.kt
@@ -0,0 +1,40 @@
+package com.mohsenoid
+
+import pi.gpio.*
+
+const val GPIO_LED = 8
+
+fun main() {
+    println("Hello world!")
+    initGPIO()
+    blink()
+}
+
+private fun initGPIO() {
+    if (gpioInitialise() < 0) {
+        println("GPIO Error initialising")
+        return
+    }
+}
+
+private fun blink() {
+    val ledPort = GPIO_LED.toUInt()
+    initPortWithMode(port = ledPort, mode = PI_OUTPUT)
+
+    println("Start blinking LED")
+    var blinkCount = 3
+    while (blinkCount > 0) {
+        gpioWrite(ledPort, PI_LOW)
+        gpioSleep(PI_TIME_RELATIVE, 0, 500000)
+        gpioWrite(ledPort, PI_HIGH)
+        gpioSleep(PI_TIME_RELATIVE, 0, 500000)
+        blinkCount--
+    }
+}
+
+private fun initPortWithMode(port: UInt, mode: Int) {
+    if (gpioSetMode(port, mode.toUInt()) < 0) {
+        println("Could not set mode for GPIO$port")
+        return
+    }
+}
