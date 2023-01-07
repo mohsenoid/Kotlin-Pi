24ac0247bd6b395b8ef988471d370ce17e05c04f
Mohsen Mirhoseini <msn.mirhoseini@gmail.com>
2023-01-07 13:56:19 +0100

    Flip Flop Game

--- src/linuxArm32HfpMain/kotlin/com/mohsenoid/Main.kt
+++ src/linuxArm32HfpMain/kotlin/com/mohsenoid/Main.kt
@@ -1,16 +1,77 @@
+@file:Suppress("UNUSED_ANONYMOUS_PARAMETER")
+
 package com.mohsenoid
 
 import kotlinx.cinterop.staticCFunction
 import pi.gpio.*
+import kotlin.properties.Delegates
 
-const val GPIO_LED = 14
-const val GPIO_BUTTON = 23
+const val GPIO_LED_S = 18
+const val GPIO_LED_1 = 15
+const val GPIO_LED_2 = 23
+const val GPIO_BUTTON_1 = 14
+const val GPIO_BUTTON_2 = 24
+
+enum class GameState {
+    IDLE, USER1, USER2
+}
+
+var gameState: GameState by Delegates.observable(GameState.IDLE) { property, oldValue, newValue ->
+    println("Game state: $newValue")
+
+    when (newValue) {
+        GameState.IDLE -> {
+            gpioWrite(GPIO_LED_1.toUInt(), PI_LOW)
+            gpioWrite(GPIO_LED_2.toUInt(), PI_LOW)
+            gpioWrite(GPIO_LED_S.toUInt(), PI_HIGH)
+        }
+
+        GameState.USER1 -> {
+            gpioWrite(GPIO_LED_1.toUInt(), PI_HIGH)
+            gpioWrite(GPIO_LED_2.toUInt(), PI_LOW)
+            gpioWrite(GPIO_LED_S.toUInt(), PI_LOW)
+        }
+
+        GameState.USER2 -> {
+            gpioWrite(GPIO_LED_1.toUInt(), PI_LOW)
+            gpioWrite(GPIO_LED_2.toUInt(), PI_HIGH)
+            gpioWrite(GPIO_LED_S.toUInt(), PI_LOW)
+        }
+    }
+}
+
+val buttonFunction = staticCFunction<Int, Int, UInt, Unit> { gpio, level, tick ->
+    when (level) {
+        1 -> {
+            if (gameState != GameState.IDLE) return@staticCFunction
+
+            gameState = when (gpio) {
+                GPIO_BUTTON_1 -> GameState.USER1
+                GPIO_BUTTON_2 -> GameState.USER2
+                else -> error("Unknown input!")
+            }
+        }
+
+        0, 2 -> {
+            if (gameState == GameState.IDLE) return@staticCFunction
+
+            when (gpio) {
+                GPIO_BUTTON_1 -> if (gameState == GameState.USER1) gameState = GameState.IDLE
+                GPIO_BUTTON_2 -> if (gameState == GameState.USER2) gameState = GameState.IDLE
+                else -> error("Unknown input!")
+            }
+
+        }
+    }
+}
 
 fun main() {
     println("Hello world!")
     initGPIO()
-    blink()
-    setupButton()
+
+    setupStatus()
+    setupButton(GPIO_BUTTON_1, GPIO_LED_1)
+    setupButton(GPIO_BUTTON_2, GPIO_LED_2)
     while (true) {
     }
 }
@@ -24,38 +85,21 @@ private fun initGPIO() {
     }
 }
 
-private fun blink() {
-    val ledPort = GPIO_LED.toUInt()
+private fun setupStatus() {
+    val ledPort = GPIO_LED_S.toUInt()
     initPortWithMode(port = ledPort, mode = PI_OUTPUT)
 
-    println("Start blinking LED")
-    var blinkCount = 3
-    while (blinkCount > 0) {
-        gpioWrite(ledPort, PI_HIGH)
-        gpioSleep(PI_TIME_RELATIVE, 0, 500000)
-        gpioWrite(ledPort, PI_LOW)
-        gpioSleep(PI_TIME_RELATIVE, 0, 500000)
-        blinkCount--
-    }
+    gpioWrite(ledPort, PI_HIGH)
 }
 
-val onButtonPressed = staticCFunction<Int, Int, UInt, Unit> { gpio, level, tick ->
-    when (level) {
-        0 -> {
-            println("Button Pressed down, level 0")
-            blink()
-        }
-
-        1 -> println("Button Released, level 1")
-        2 -> println("Button GPIO timeout, no level change")
-    }
-}
-
-private fun setupButton() {
-    val buttonPort = GPIO_BUTTON.toUInt()
+private fun setupButton(buttonIo: Int, ledIo: Int) {
+    val buttonPort = buttonIo.toUInt()
     initPortWithMode(buttonPort, PI_INPUT)
 
-    gpioSetAlertFunc(buttonPort, onButtonPressed)
+    val ledPort = ledIo.toUInt()
+    initPortWithMode(port = ledPort, mode = PI_OUTPUT)
+
+    gpioSetAlertFunc(buttonPort, buttonFunction)
 }
 
 private fun initPortWithMode(port: UInt, mode: Int) {
